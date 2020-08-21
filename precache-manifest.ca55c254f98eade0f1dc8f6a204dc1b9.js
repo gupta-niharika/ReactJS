fadf703eedba5f77765faf01242b650e3d615b6c
Nikki <niharikap.gupta2019@vitstudent.ac.in>
2020-08-21 09:30:04 +0530

    Updates

---
+++ precache-manifest.ca55c254f98eade0f1dc8f6a204dc1b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c1a4fedce6a423118405afa34386f9",
+    "url": "/gupta-niharika/ReactJS/index.html"
+  },
+  {
+    "revision": "0bb54ffa492641b39a6b",
+    "url": "/gupta-niharika/ReactJS/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "001f0fb8c375fd8a6d43",
+    "url": "/gupta-niharika/ReactJS/static/js/2.86c41a82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gupta-niharika/ReactJS/static/js/2.86c41a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb54ffa492641b39a6b",
+    "url": "/gupta-niharika/ReactJS/static/js/main.fb86466c.chunk.js"
+  },
+  {
+    "revision": "369f3915ad1f43fc6643",
+    "url": "/gupta-niharika/ReactJS/static/js/runtime-main.0da8fc92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gupta-niharika/ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
